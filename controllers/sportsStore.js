89b30a62d84220213dac911a12a49b82b75f0740
Olga Helena <oh.cardenas@gmail.com>
2014-09-07 19:26:58 -0500

    created an inherited controller

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,12 +1,10 @@
 angular.module('sportsStore').controller('sportsStoreCntrl', function($scope){
     $scope.data = {
         products: [
-            {name:"sksks", category: "category 1", price:100},
-            {name:"sksks", description:"ssl lsls ls", category: "category 1", price:100}
+            {name:"sksk1", category: "category 1", price:100},
+            {name:"sksk2", description:"ssl lsls ls", category: "category 1", price:100},
+            {name:"sksk3", description:"ssl lsls ls", category: "category 2", price:100},
+            {name:"sksk4", description:"ssl lsls ls", category: "category 2", price:100}
         ]
-    }
-    $scope.selectCategory = function(item)
-    {
-
     }
 });
